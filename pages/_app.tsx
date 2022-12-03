be2961b7572c3cd480750c91b00c67319ac7056f
julienvanbeveren <julien.vanbeveren@gmail.com>
2022-12-03 20:40:09 +0100

    fixed styling of code blocks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../src/styles/normalize.scss'
 import '../src/styles/index.scss'
 import '../src/styles/blogpost.scss'
+import 'highlight.js/scss/a11y-dark.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
